b25b1ea9118ac42685aad0ea6d7abce929c35142
pestrada <paul.estrada@crowdint.com>
2012-09-24 18:00:22 -0500

    added stock field and validations in Product model

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -5,4 +5,12 @@ module ProductsHelper
     number_to_currency price
   end
   
+  def print_stock(stock)
+    if stock > 0
+      return stock
+    else
+      return "Out of Stock"
+    end
+  end
+  
 end
