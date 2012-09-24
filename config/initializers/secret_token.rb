68da5b63cbc30893602a1ccb69e5de44cb463784
pestrada <paul.estrada@crowdint.com>
2012-09-24 10:09:22 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Emerchant::Application.config.secret_token = '40e4c93384fad08e90982a36eec03794802fbc1fc68485b8570c2bb66f91e426cfda0fad37b3ac31631c375010f2d0e518a64ce482db2acb9f56df98d9cf0db9'
