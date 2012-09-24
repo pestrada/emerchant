79db4f8de20abec353d0fa131d1ece0f9827e418
pestrada <paul.estrada@crowdint.com>
2012-09-24 16:20:05 -0500

    removed dollar sign in prices

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ActiveRecord::Base
   attr_accessible :title, :price, :description, :image_url
   validates_numericality_of :price
+  
+  def price=(input)
+    input.delete!("$")
+    super
+  end
+  
 end
