996742eeec3fdd7a81a55ad2b90855c08a7a75be
pestrada <paul.estrada@crowdint.com>
2012-09-25 12:11:31 -0500

    fixed flash messages

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -22,8 +22,12 @@ class OrderItemsController < ApplicationController
 
   def update
     @order_item = OrderItem.find(params[:id])
-    if @order_item.update_attributes(params[:order_item])
-      redirect_to @order_item, :notice  => "Successfully updated order item."
+    if params[:order_item][:quantity].to_i == 0
+      @order_item.destroy
+      flash[:notice] = "Item #{@order_item.product.title} was removed!"
+      redirect_to @order_item.order
+    elsif @order_item.update_attributes(params[:order_item])
+      redirect_to @order_item.order, :notice => "Successfully updated order item."
     else
       render :action => 'edit'
     end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,6 +3,8 @@ class OrderItem < ActiveRecord::Base
   belongs_to :order
   belongs_to :product
   validates_presence_of :product_id, :order_id
+  validates_numericality_of :quantity, :only_integer => true, :greater_than => 0
+  
   
   def subtotal
     self.quantity * self.product.price
